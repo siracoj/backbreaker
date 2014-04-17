357e410716dfd1a581c2847726ea12814496ab58
siracoj <siraco.jared@gmail.com>
2014-04-17 05:20:19 -0400

    Working graph

--- AudrinoCode/PressureSensorReadTest/PressureSensorReadTest.ino
+++ AudrinoCode/PressureSensorReadTest/PressureSensorReadTest.ino
@@ -1,8 +1,7 @@
-#include <SD.h>
-
 //Audrino code to read in the six sensors
 //Reads data every 100ms
 float sensorValues[4];
+float voltConvert = (5.0 / 1023.0);
 void setup()
 {
   //start Serial
@@ -11,34 +10,39 @@ void setup()
 
 void loop()
 {
-  //reading in sensor data from 6 analog sensors
+  //reading in sensor data from 4 analog sensors
 
-  sensorValues[0] = analogRead(A2);
-  sensorValues[1] = analogRead(A3);
-  sensorValues[2] = analogRead(A4);
-  sensorValues[3] = analogRead(A5);
-  
-  /*
-  if(SD.begin()){ //Writing data to the SD card
+  sensorValues[0] = analogRead(A2) * voltConvert;
+  sensorValues[1] = analogRead(A3) * voltConvert;
+  sensorValues[2] = analogRead(A4) * voltConvert;
+  sensorValues[3] = analogRead(A5) * voltConvert;
+ 
+ /* 
+  if(!SD.begin(2)){ //Writing data to the SD card
+    Serial.write("Failed to read SD\n");
+  }else{
     File data = SD.open("sensordata.csv", FILE_WRITE);
-    String row = "";
-    for(int i = 0; i < 5; i++)
+    char row[9] = "";
+    for(int i = 0; i < 7; i++)
     {
-      row = row + sensorValues[i] + ","; //creating a comma separated file
+      row[i] = sensorValues[i];
+      i++;
+      row[i] = ','; //creating a comma separated file
     }
-    row = row + sensorValues[5] + "\n";
-    char writeData[row.length()];
-    row.toCharArray(writeData, row.length()); //conversion to char array
-    data.write(writeData);
+    row[7] = sensorValues[3]; 
+    row[8] = '\n';
+    data.write(row);
+    Serial.write(data.read());
     data.close();
-  }*/
+  }
+  */
   Serial.print(sensorValues[0]);
-  Serial.print(" | ");
+  Serial.print(" ");
   Serial.print(sensorValues[1]);
-  Serial.print(" | ");
+  Serial.print(" ");
   Serial.print(sensorValues[2]);
-  Serial.print(" | ");
-  Serial.println(sensorValues[3]);
-  delay(1000); //wait 100ms
-    
+  Serial.print(" ");
+  Serial.println(sensorValues[3]); 
+  delay(50); //wait 100ms
+
 }
